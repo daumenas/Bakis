eb7c8cb316de982479e32892114ff47d39a6bbc9
daumenas <daumius@gmail.com>
2020-03-07 15:24:55 +0200

    Initial template
    template push

---
+++ Bakis/Controllers/ControllerExample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Bakis.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ControllerExample : ControllerBase
+    {
+
+        private readonly ILogger<ControllerExample> _logger;
+
+        public ControllerExample(ILogger<ControllerExample> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
