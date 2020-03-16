ee245ae860d8fedb2bf62ec4d8907fd7be14e9a2
Povilas <povilas.kondr@gmail.com>
2020-03-16 19:21:41 +0200

    FormControl add

--- Bakis/ClientApp/src/app/components/register/register.component.ts
+++ Bakis/ClientApp/src/app/components/register/register.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroupDirective, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-register',
@@ -7,10 +16,22 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class RegisterComponent implements OnInit {
+  minDate: Date;
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+  passwordFormControl = new FormControl('', [
+    Validators.required
+  ]);
+
+  matcher = new MyErrorStateMatcher();
 
   Roles: any = ['User', 'Event Organizer'];
 
-  constructor() { }
+  constructor() {
+    const currentYear = new Date().getFullYear();
+    this.minDate = new Date(currentYear - 120, 0, 1);}
 
   ngOnInit() {
   }
