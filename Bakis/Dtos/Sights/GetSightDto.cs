298224e80789b9a363e9d6a09fad3635702a26f3
daumenas <daumius@gmail.com>
2020-03-31 19:21:02 +0300

    Fix naming issues

--- Bakis/Dtos/Sights/GetSightDto.cs
+++ Bakis/Dtos/Sights/GetSightDto.cs
@@ -13,6 +13,6 @@ namespace Bakis.Dtos.Sights
         public double Points { get; set; }
         public string Address { get; set; }
         public double Latitude { get; set; }
-        public double Langtitude { get; set; }
+        public double Longitude { get; set; }
     }
 }
