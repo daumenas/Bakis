c66ab37ae0e73a6c690b404cc731ff9c70953972
Povilas <povilas.kondr@gmail.com>
2020-04-08 15:58:53 +0300

    Added play game functionality
    Activates when you are 30m near

--- Bakis/ClientApp/src/app/components/map/map.component.ts
+++ Bakis/ClientApp/src/app/components/map/map.component.ts
@@ -17,16 +17,20 @@ export class MapComponent implements AfterViewInit  {
   events: TableRowEvent[];
   listOfSightData: TableRowSight[] = [];
   sights: TableRowSight[];
+  private location;
+  private sightsMarkers = [];
+  private eventMarkers = [];
 
   constructor(
     private eventService: CityEventService,
-    private sightService: LocationService) { }
+    private sightService: LocationService,) { }
 
   ngAfterViewInit(): void {
     let map = L.map('map', {
       center: [54.896870, 23.886105],
       zoom: 15
     });
+
     this.initMap(map);
     this.eventService.getAllEvents().subscribe(events => {
       this.events = events;
@@ -58,32 +62,58 @@ export class MapComponent implements AfterViewInit  {
       maxZoom: 120
     }).on("locationfound", e => {
       if (!userLocation) {
-        userLocation = new L.marker(e.latlng).addTo(map);
+        userLocation = new L.marker(e.latlng, { title: 'position' }).addTo(map);
+        this.location = e.latlng;
+        userLocation.on('move', e => {
+          this.getGameDistance(map);
+        })
       } else {
+        this.location = e.latlng;
         userLocation.setLatLng(e.latlng);
+        
       }
     }).on("locationerror", error => {
       if (userLocation) {
         map.removeLayer(userLocation);
         userLocation = undefined;
       }
-    });
+    })
   }
 
   setEventMarkers(map) {
-  for (var i = 0; i < this.listOfEventData.length; i++) {
-    L.marker([this.listOfEventData[i].latitude, this.listOfEventData[i].longitude]).addTo(
-      map).bindPopup('<p>' + this.listOfEventData[i].name + '<br />' + this.listOfEventData[i].description + '</p>'
+    for (var i = 0; i < this.listOfEventData.length; i++) {
+      this.eventMarkers[i] = L.marker([this.listOfEventData[i].latitude, this.listOfEventData[i].longitude]).addTo(
+        map).bindPopup('<p>' + this.listOfEventData[i].name + '<br />' + this.listOfEventData[i].description + '</p>'
       );
   }
 }
 
   setSightMarkers(map) {
-  for (var i = 0; i < this.listOfSightData.length; i++) {
-    L.marker([this.listOfSightData[i].latitude, this.listOfSightData[i].longitude]).addTo(
-      map).bindPopup('<p>' + this.listOfSightData[i].name + '<br />' + this.listOfSightData[i].description + '</p>'
+    for (var i = 0; i < this.listOfSightData.length; i++) {
+      this.sightsMarkers[i] = L.marker([this.listOfSightData[i].latitude, this.listOfSightData[i].longitude]).addTo(
+        map).bindPopup('<p>' + this.listOfSightData[i].name + '<br />' + this.listOfSightData[i].description + '</p>'
+        + '<button disabled>Play game</button>'
       );
   }
-}
+  }
+
+  async getGameDistance(map) {
+    if (this.sightsMarkers.length == 0) {
+      setTimeout(() => { this.getGameDistance(map) }, 3000);
+    }
+    for (var i = 0; i < this.sightsMarkers.length; i++) {
+      var meters = this.location.distanceTo(this.sightsMarkers[i]._latlng);
+      if (meters <= 30) {
+        this.sightsMarkers[i]._popup.setContent('<p>' + this.listOfSightData[i].name + '<br />' + this.listOfSightData[i].description + '</p>'
+          + '<button>Play game</button>');
+        this.sightsMarkers[i].update();
+      }
+      else {
+        this.sightsMarkers[i]._popup.setContent('<p>' + this.listOfSightData[i].name + '<br />' + this.listOfSightData[i].description + '</p>'
+          + '<button disabled>Play game</button>');
+        this.sightsMarkers[i].update();
+      }
+    }
+  }
 
 }
